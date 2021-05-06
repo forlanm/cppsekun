8626ea2a56166ed6d9832e006104b26f11472011
olanro-ux <4lanmsqd@gmail.com>
2021-05-06 16:09:38 +0300

    yea commit

---
+++ for windows/code.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <windows.h>
+using namespace std;
+int main ()
+{
+	int sec = 0;
+	int min = 0;
+	int hou = 0;
+	for (;;)
+	{
+		cout<<"	||	Прошло  "<<hou<<" часов "<<min<<" минут "<<sec<< " секунд   || "<<endl;
+		sec++;
+		Sleep(1);
+		if (sec == 60 ){
+			min += 1;
+			sec = 0;
+			}
+		if (min == 60)
+			{
+				hou += 1;
+			}
+	}
+
+}
